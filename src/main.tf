cb3eac246bd2f2fca59d212c62f574376d4fd24d
allanShady <acamilo.ag@gmail.com>
2022-03-15 17:52:11 +0200

    wip: add terraform outputs

--- src/main.tf
+++ src/main.tf
@@ -133,3 +133,11 @@ resource "aws_instance" "web-server-instance" {
     Name = "tf-web-server"
   }
 }
+
+output "server_private_ip" {
+  value = aws_instance.web-server-instance.private_ip
+}
+
+output "server_id" {
+  value = aws_instance.web-server-instance.id
+}
