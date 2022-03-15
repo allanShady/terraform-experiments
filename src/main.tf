57a816cfec5f94bfed929623fabe914816a96d8d
allanShady <acamilo.ag@gmail.com>
2022-03-15 17:34:27 +0200

    feat: real world resourse provision sample

--- src/main.tf
+++ src/main.tf
@@ -3,18 +3,133 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_vpc" "first-vpc" {
+# 1. Create a VPC
+resource "aws_vpc" "dev-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "prodution"
+    Name = "dev"
   }
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.first-vpc.id
-  cidr_block = "10.0.1.0/24"
+# 3. Create internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.dev-vpc.id
+}
+
+# 3. Create a custom route table
+resource "aws_route_table" "dev-route-table" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
 
   tags = {
-    Name = "prod-subnet"
+    Name = "dev"
+  }
+}
+
+# 4. Create a Subnet
+resource "aws_subnet" "subnert-1" {
+  vpc_id            = aws_vpc.dev-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "dev-subnet"
+  }
+}
+
+# 5. Associate subnet with Route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnert-1.id
+  route_table_id = aws_route_table.dev-route-table.id
+}
+
+# 6. Create a security Group to allow port 22, 80 and 443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+
+  vpc_id = aws_vpc.dev-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "Allow_web_traffic"
+  }
+}
+
+# 7. Create a newtwork interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnert-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+# 8. Assign an elastic IP to the network interface created in step 7
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [
+    aws_internet_gateway.gw
+  ]
+}
+
+# 9. Create ubuntu server and install/enable apache
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-085925f297f89fce1"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install apache2 -y
+              sudo systemctl start apache2
+              sudo bash -c 'echo FIRST WEB SERVER USING TF and AWS > /var/www/html/index.html'
+              EOF
+  tags = {
+    Name = "tf-web-server"
   }
 }
